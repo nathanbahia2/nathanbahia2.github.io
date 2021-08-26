b38cb8bb8ea998fb4be19b093458465c8142a53f
nathanbahia2 <nathan@datatype.com.br>
2021-08-26 18:22:26 -0300

    [STYLE] - melhoria no layout do site

---
+++ src/js/projetos.js
@@ -0,0 +1,34 @@
+const projetos = [
+    {
+        titulo: "Portfólio",
+        descricao: "Exemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projetoExemplo de projeto",
+        imagem_url: "https://nathanbahia.github.io/src/images/projetos/P-003%20-%20Rel%C3%B3gio%20Digital.png",
+        tecnologias: [],
+        github_link: "https://...",
+        projeto_link: "https://..."
+    },
+    {
+        titulo: "Outro projeto",
+        descricao: "Exemplo de projeto",
+        imagem_url: "https://nathanbahia.github.io/src/images/projetos/P-003%20-%20Rel%C3%B3gio%20Digital.png",
+        tecnologias: [],
+        github_link: "https://...",
+        projeto_link: "https://..."
+    },
+    {
+        titulo: "Outro projeto",
+        descricao: "Exemplo de projeto",
+        imagem_url: "https://nathanbahia.github.io/src/images/projetos/P-003%20-%20Rel%C3%B3gio%20Digital.png",
+        tecnologias: ["Python", "Javascript", "Bootstrap"],
+        github_link: "",
+        projeto_link: "https://..."
+    },
+    {
+        titulo: "Outro projeto",
+        descricao: "Exemplo de projeto",
+        imagem_url: "https://nathanbahia.github.io/src/images/projetos/P-003%20-%20Rel%C3%B3gio%20Digital.png",
+        tecnologias: ["Python", "Javascript", "Bootstrap", "JQuery", "Django"],
+        github_link: "",
+        projeto_link: ""
+    },
+]
